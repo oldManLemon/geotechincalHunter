1790e1a9a269107aec455e1b356864604872a27b
hase <oldm8reggie@gmail.com>
2018-11-29 15:51:14 +1000

    isold needs more work

--- folderRunner.py
+++ folderRunner.py
@@ -2,10 +2,10 @@
 import os
 
 base = "\\\hawkeye\\archive\\"
-yearFolder = '2015\\'
+yearFolder = '2017\\'
 job = '15083'
 
-combined = base+yearFolder+job
+combined = base+yearFolder
 
 def isOld(path):
     """ Check to see if folder is of the old type.
@@ -16,14 +16,46 @@ def isOld(path):
     Returns:
         bool"""
     for dirs in os.listdir(path):
-        print(dirs)
+        print('Looking at ',dirs)
+
         if(dirs.startswith('0')):
-           return print("This is new Style")
+            print("False, ie it is new")
+            return False
+        elif(dirs.startswith("_")):
+            print("this has a suffix")
+        #     findParents(path)   
 
+    
 
+def findParents(path):
+    """ Find Parent folders
+    
+    Args:
+        path (str)
+    
+    Returns:
+        dunno yet"""
+    for jobNumber in os.listdir(path): #get job number and enter
+        jobNumber = str(jobNumber)
+        #print(path+jobNumber)
+        isOld(path+jobNumber)
+        if isOld == False:
+            print(jobNumber, "Is New style")
+            m = jobNumber
+        elif isOld == True:
+            print(jobNumber + " Is is old school")
+        else: 
+            print("Unidentified")        
+        #print(jobNumber)
+        #show Contents
+        # for data in os.listdir(path+jobNumber):
+        #     fullpath = path+jobNumber+'\\'+data
+        #     print(fullpath)
+            
+        
 
-
-isOld(combined)
-print(combined)
+isOld(combined+"17208")
+#findParents(combined)
+#print(combined)
 
 
